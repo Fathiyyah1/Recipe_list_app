530a273b4e27ec57d5822f9e766914ad71108fa5
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-06-16 02:19:28 +0300

    .

---
+++ lib/models/recipe_details.api.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'recipe_details.dart';
+// const req = unirest('GET', 'https://yummly2.p.rapidapi.com/feeds/search');
+
+// req.query({
+// 	start: '0',
+// 	maxResult: '18',
+// 	maxTotalTimeInSeconds: '7200',
+// 	q: 'chicken soup',
+// 	allowedAttribute: 'diet-lacto-vegetarian,diet-low-fodmap',
+// 	FAT_KCALMax: '1000'
+// });
+
+// req.headers({
+// 	'x-rapidapi-key': '37373ffb6cmsh9b25211f7bd423ap17eb7djsn3ef767d4d809',
+// 	'x-rapidapi-host': 'yummly2.p.rapidapi.com'
+// });
+class RecipeDetailsApi {}
