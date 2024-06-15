01964ce11e7941efa8e037ff8bda2bcf057c5283
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-06-15 20:48:50 +0300

    ..

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -10,6 +10,16 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(Icons.restaurant_menu),
+            SizedBox(width: 10),
+            Text('Food Recipe'),
+          ],
+        ),
+      ),
+    );
   }
 }
