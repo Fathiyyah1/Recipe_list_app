36208ce212fd4ef9aea2ea2607c81e8a7601b92b
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-06-15 20:15:47 +0300

    hi

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'API Series',
+      debugShowCheckedModeBanner: false,
+      home: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
