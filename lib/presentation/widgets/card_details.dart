dd475c1d5860e4f47d67bc5b390f1db3140dd306
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-06-16 07:35:16 +0300

    ..

--- lib/presentation/widgets/card_details.dart
+++ lib/presentation/widgets/card_details.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 Widget CardDetails(
-    {
-    //   required String name,
-    // required String cookTime,
-    // required String rating,
+    {required String name,
+    required String cookTime,
+    required String rating,
+    required String displayName,
     required String image}) {
   return Container(
     margin: EdgeInsets.symmetric(horizontal: 22, vertical: 10),
@@ -33,6 +33,13 @@ Widget CardDetails(
         fit: BoxFit.cover,
       ),
     ),
-    child: Row(),
+    child: Column(
+      children: [
+        Text(name),
+        Text(cookTime),
+        Text(rating),
+        Text(displayName),
+      ],
+    ),
   );
 }
