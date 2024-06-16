6b25b90bbd3318bc1510949428b29349cb942e83
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-06-16 03:37:21 +0300

    .

--- lib/presentation/widgets/card_details.dart
+++ lib/presentation/widgets/card_details.dart
@@ -6,6 +6,31 @@ Widget CardDetails(
     required String rating,
     required String thumbnailUrl}) {
   return Container(
+    margin: EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+    height: 180,
+    decoration: BoxDecoration(
+      color: Colors.black,
+      borderRadius: BorderRadius.circular(15),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.6),
+          offset: Offset(
+            0.0,
+            10.0,
+          ),
+          blurRadius: 10.0,
+          spreadRadius: -6.0,
+        ),
+      ],
+      image: DecorationImage(
+        colorFilter: ColorFilter.mode(
+          Colors.black.withOpacity(0.35),
+          BlendMode.multiply,
+        ),
+        image: NetworkImage(thumbnailUrl),
+        fit: BoxFit.cover,
+      ),
+    ),
     child: Column(
       children: [
         Image.network(thumbnailUrl),
