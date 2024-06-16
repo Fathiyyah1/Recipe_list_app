94356c26b3c0ff4c59eeb9b6a26cf3c552b825a1
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-06-16 03:32:01 +0300

    .

--- lib/presentation/widgets/card_details.dart
+++ lib/presentation/widgets/card_details.dart
@@ -1,5 +1,18 @@
+import 'package:flutter/material.dart';
 
-
-Widget CardDetails {
-
+Widget CardDetails(
+    {required String title,
+    required String cookTime,
+    required String rating,
+    required String thumbnailUrl}) {
+  return Container(
+    child: Column(
+      children: [
+        Image.network(thumbnailUrl),
+        Text(title),
+        Text(cookTime),
+        Text(rating),
+      ],
+    ),
+  );
 }
